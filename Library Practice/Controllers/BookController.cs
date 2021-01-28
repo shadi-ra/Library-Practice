6f2daa8bc825f498d299c962817b2734dbe5ddc4
shadi_rahbar74@yahoo.com <shadi_rahbar74@yahoo.com>
2021-01-28 14:26:50 +0330

    ...

--- Library Practice/Controllers/BookController.cs
+++ Library Practice/Controllers/BookController.cs
@@ -61,50 +61,71 @@ namespace Library_Practice.Controllers
         {
            
 
-            int publisherId = 0;
-            var categoryIds = categoryRepository.GetAll().Where(x => input.categories.Contains(x.Name)).Select(x => x.Id).ToList();
 
-            var authorIds = authorRepository.GetAll().Where(x => input.authors.Contains(x.FullName)).Select(x => x.Id).ToList();
+            var resp1 = bookCategoryRepository.GetQuery()
+                .Include(x => x.Book)
+                .Include(x => x.Category)
+                .Include(x => x.Book.publisherr)
+                .Include(x => x.Book.BookAuthors).ThenInclude(x => x.Author)
 
-            var publisher = publisherRepository.GetAll().Where(x => x.Name == input.publication).FirstOrDefault();
-            if (publisher != null)
-            {
-                publisherId = publisher.Id;
-            }
-
-
-            var bookId1 = bookCategoryRepository.GetAll().Where(x => categoryIds.Contains(x.CategoryId)).Select(x => x.BookId).ToList();
-
-            var bookId2 = BookAuthorRepository.GetAll().Where(x => authorIds.Contains(x.AuthorId)).Select(x => x.BookId).ToList();
-
-            bookId1.AddRange(bookId2);
-
-            List<Book> books = new List<Book>();
-            if (bookId1.Count != 0)
-            {
-                books = bookRepository.GetAll().Where(x => bookId1.Contains(x.Id)).ToList();
-            }
-            if (publisherId != 0)
-            {
-                var books1 = bookRepository.GetAll().Where(x => bookId1.Contains(x.Id) && x.PublisherrId == publisherId).ToList();
-                books.AddRange(books1);
-            }
-
-            if (books.Count != 0) 
-            {
-               var resp = books.Select(x => new SearchResponse()
+                .Where(x => input.categories.Contains(x.Category.Name)
+                           || input.authors.Contains(x.Book.BookAuthors.Select(z => z.Author.FullName).FirstOrDefault())
+                           || x.Book.publisherr.Name == input.publication)
+               . Select(x => new SearchResponse()
                 {
-                    title = x.Title,
-                    authors = x.BookAuthors.Select(x => x.Author.FullName).ToList(),
-                    publishDate = x.PublishDate,
-                    publisher = x.publisherr.Name,
-                    ISBN = x.ISBN
+                    title = x.Book.Title,
+                    authors = x.Book.BookAuthors.Select(x => x.Author.FullName).ToList(),
+                    publishDate = x.Book.PublishDate,
+                    publisher = x.Book.publisherr.Name,
+                    ISBN = x.Book.ISBN
                 }).ToList();
+            return new SearchListBookResponse() { Books = resp1 };
 
-                return new SearchListBookResponse() { Books = resp };
-            }
-            return null;
-      
+
+            //int publisherId = 0;
+            //var categoryIds = categoryRepository.GetAll().Where(x => input.categories.Contains(x.Name)).Select(x => x.Id).ToList();
+
+            //var authorIds = authorRepository.GetAll().Where(x => input.authors.Contains(x.FullName)).Select(x => x.Id).ToList();
+
+            //var publisher = publisherRepository.GetAll().Where(x => x.Name == input.publication).FirstOrDefault();
+            //if (publisher != null)
+            //{
+            //    publisherId = publisher.Id;
+            //}
+
+
+            //var bookId1 = bookCategoryRepository.GetAll().Where(x => categoryIds.Contains(x.CategoryId)).Select(x => x.BookId).ToList();
+
+            //var bookId2 = BookAuthorRepository.GetAll().Where(x => authorIds.Contains(x.AuthorId)).Select(x => x.BookId).ToList();
+
+            //bookId1.AddRange(bookId2);
+
+            //List<Book> books = new List<Book>();
+            //if (bookId1.Count != 0)
+            //{
+            //    books = bookRepository.GetAll().Where(x => bookId1.Contains(x.Id)).ToList();
+            //}
+            //if (publisherId != 0)
+            //{
+            //    var books1 = bookRepository.GetAll().Where(x => bookId1.Contains(x.Id) && x.PublisherrId == publisherId).ToList();
+            //    books.AddRange(books1);
+            //}
+
+            //if (books.Count != 0) 
+            //{
+            //   var resp = books.Select(x => new SearchResponse()
+            //    {
+            //        title = x.Title,
+            //        authors = x.BookAuthors.Select(x => x.Author.FullName).ToList(),
+            //        publishDate = x.PublishDate,
+            //        publisher = x.publisherr.Name,
+            //        ISBN = x.ISBN
+            //    }).ToList();
+
+            //    return new SearchListBookResponse() { Books = resp };
+            //}
+            //return null;
+
         }
         [HttpPut]
         public void Update([FromBody] Book input)
